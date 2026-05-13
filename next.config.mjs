2bec88db76c69fc07c82beb4f21153f988a9b7f2
Filo Alvian Ongky <filoalvianongky@gmail.com>
2026-05-14 00:58:06 +0700

    Fix config for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  devIndicators: false, // Langsung set false di sini
+  devIndicators: false, // Settingan lama kamu tetap ada
+  output: 'export',     // Tambahan: Agar Next.js bisa jadi file statis
+  images: {
+    unoptimized: true,  // Tambahan: Wajib supaya logo medsos kamu muncul di GitHub Pages
+  },
 };
 
 export default nextConfig;
